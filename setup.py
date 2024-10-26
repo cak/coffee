75bd60f096fca61a0b727968068784df27233f38
Caleb Kinney <cak@typeerror.com>
2024-10-26 12:53:57 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='coffee',
+    version='0.0.1',
+    author='Caleb Kinney',  # Replace with your actual name
+    author_email='caleb@typerror.com',
+    description='A placeholder package to reserve the name "coffee" on PyPI.',
+    long_description='This is a placeholder package for a future project.',
+    long_description_content_type='text/plain',
+    url='https://github.com/cak/coffee',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
